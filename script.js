de89cdaebd6bbcd6edaeb8cae2d6ccb4b1402151
David Nyakinyua <davynyakinyua@gmail.com>
2024-08-15 12:58:36 +0300

    Select the div and add a function to create a divs on the dom

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log('Hello world');
+
+
+const container = document.querySelector('div');
+
+
+let input = prompt('Enter the number of squares to display on the grid');
+
+console.log(input);
+
+function grid(){
+
+    const square = document.createElement('div');
+    square.classList.toggle('sqr');
+    container.appendChild(square);
+
+    square.addEventListener('mouseover', (event) => {
+        console.log(event.type);
+
+        if(event.type === 'mouseover'){
+            square.classList.toggle('toggle');
+        }
+    });
+
+};
+
+for(let i = 0; i < input; i++){
+    grid();
+};
+
+
