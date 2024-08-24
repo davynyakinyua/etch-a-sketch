3731a22aee53e5a7972cf26d7280b1970c8b36b0
David Nyakinyua <davynyakinyua@gmail.com>
2024-08-24 16:32:01 +0300

    Add an event listener, some conditions and tidy up

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
 console.log('Hello world');
 
+
+//create two variables button and container and refer them to html
 const container = document.querySelector('div');
 const button = document.querySelector('button');
 
 
-function grid(){
+let sqrNum = 28;
 
-    const square = document.createElement('div');
-    square.classList.toggle('sqr');
-    container.appendChild(square);
+//create a function square that creates squares on the html dom
+function grid(numChosen){
+    
+    let num = numChosen * numChosen;
+    
+    for(let i = 1; i <= num; i++){
+        const div = document.createElement('div');
+        div.classList.toggle('square');
+        container.appendChild(div);
 
-    square.addEventListener('mouseover', (event) => {
-        console.log(event.type);
+        function hover(){
+        div.classList.toggle('hover');
 
-        if(event.type === 'mouseover'){
-            square.classList.toggle('toggle');
-        }
-    });
+        };
+
+        div.addEventListener('mouseover', hover); 
+    };
 
 };
 
 
+function reload(){
 
-button.addEventListener('click', (event) => {
-
-    console.log(event.type);
-
-    if(event.type === 'click'){
-
-        //document.location.reload();
-
-        let input = prompt('Enter the number of squares to display on the grid');
-
-        console.log(input);
-
-        for(let i = 0; i < input; i++){
+    document.location.reload();
     
-            grid();
-        
-        };
+};
 
-    };
-});
+function choice(){
+    sqrNum = prompt('Enter a number between 10 and 28');
+
+    console.log(sqrNum);
+    if(sqrNum < 10 || sqrNum > 28){
+        grid(28);
+        alert('please enter a number between 10 and 28');
+    } else if(sqrNum > 9 && sqrNum < 29){
+        grid(sqrNum);
+    } else{
+        grid(28);
+        alert('Error! Please enter a number between 10 and 28');
+    }
+
+    
+}
+
+
+button.addEventListener('click', reload);
+addEventListener('DOMContentLoaded', choice);
+
+//grid(sqrNum);
